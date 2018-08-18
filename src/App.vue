0890d98243c27d2e0ce229955de7f52fd8d76eea
Oleksandr Bystrikov <coder@yakviter.com.ua>
2018-08-18 05:11:55 +0200

    feat(core): define app structure

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,51 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="App container">
+    <TheHeader />
+    <Content />
+    <TheFooter />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+import TheHeader from '@/components/layout/TheHeader'
+import Content from '@/components/layout/Content'
+import TheFooter from '@/components/layout/TheFooter'
+
+export default {
+  name: 'App',
+
+  components: {
+    TheHeader,
+    Content,
+    TheFooter
+  }
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+</script>
+
+<style src="@/assets/css/global.scss" lang="scss"></style>
+<style lang="scss" scoped>
+/*
+  Main css class === component name.
+  Always belongs to component's top container
+*/
+.App {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  height: inherit;
+  box-sizing: border-box;
+
+  .TheHeader {
+    flex: 1 0 auto;
+  }
+
+  .Content {
+    flex: 10 0 auto;
+  }
+
+  .TheFooter {
+    flex: 1 0 auto;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App'
 
+import router from '@/router'
+import store from '@/store' // vuex
+import plugins from '@/plugins'
+
+Vue.use(plugins)
 Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
